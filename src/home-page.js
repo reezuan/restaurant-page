9c35cd1042549d5cf5d843c3a958a615c499cb8e
Rizuan <john.lee.coco@gmail.com>
2024-01-23 02:19:09 +0800

    Add hero image & overlay
    - Haven't properly styled it yet, though.

--- src/home-page.js
+++ src/home-page.js
@@ -1,11 +1,31 @@
+import CatBackground from "./cat-hero-img.jpeg";
+
 export default function homePage() {
     const content = document.querySelector("#content");
+
+    const heroSection = document.createElement("section");
+    const heroImage = new Image();
+    const overlay = document.createElement("div");
     const pageTitle = document.createElement("h1");
     const pageSubTitle = document.createElement("h2");
+    
+    heroSection.classList.add("hero");
+    
+    heroImage.src = CatBackground;
+    heroImage.classList.add("hero-image");
 
-    pageTitle.innerHTML = `<span>Your Meowjesty's</span><br><span>Seefood Restaurant</span>`;
+    overlay.classList.add("overlay");
+
+    pageTitle.innerHTML = `<span>Your Meowjesty's</span><br><span>Seef👀d Restaurant</span>`;
     pageSubTitle.textContent = "You can have anything you want! (as long as it's tuna)";
 
-    content.appendChild(pageTitle);
-    content.appendChild(pageSubTitle);
+    overlay.appendChild(pageTitle);
+    overlay.appendChild(pageSubTitle);
+
+    // Append content to .hero section
+    heroSection.appendChild(heroImage);
+    heroSection.appendChild(overlay);
+
+    // Append .hero section to #content
+    content.appendChild(heroSection);
 };
