673da8cfcb3889a41db1156a78e648d3ec2155a9
Rizuan <john.lee.coco@gmail.com>
2024-01-26 01:38:35 +0800

    Enable changing of pages by clicking on nav links

--- src/about-us-page.js
+++ src/about-us-page.js
@@ -1,3 +1,7 @@
 export default function aboutUsPage() {
+    const testText = document.createElement("h3");
+    const content = document.querySelector("#content");
 
+    testText.textContent = "Hello";
+    content.appendChild(testText);
 };
--- src/init-page.js
+++ src/init-page.js
@@ -1,12 +1,18 @@
 import Logo from "./cat-with-fish.png";
+import _ from "lodash";
+import homePage from "./home-page.js";
+import aboutUsPage from "./about-us-page.js";
+import contactPage from "./contact-page.js";
 
 export default function initPage() {
-    // Create & append header
     const header = document.createElement("header");
+    const content = document.createElement("section");
+    const siteLogo = new Image();
+
+    // Create & append header
     document.body.appendChild(header);
     
     // Create & append logo
-    const siteLogo = new Image();
     siteLogo.src = Logo;
     siteLogo.classList.add("logo");
     header.appendChild(siteLogo);
@@ -22,14 +28,37 @@ export default function initPage() {
         const link = document.createElement("li");
         link.textContent = navLink;
         link.classList.add("nav-link");
+        link.id = `${_.kebabCase(navLink)}-page`;
         navList.append(link);
     });
 
     navBar.appendChild(navList);
     header.appendChild(navBar);
 
+    // Append event listeners to navbar links
+    const navPages = document.querySelectorAll(".nav-link");
+    
+    navPages.forEach(navPage => {
+        navPage.addEventListener("click", (event) => {
+            content.innerHTML = ``;
+
+            console.log(event.target);
+
+            switch (event.target.id) {
+                case "home-page":
+                    homePage();
+                    break;
+                case "about-us-page":
+                    aboutUsPage();
+                    break;
+                case "contact-us-page":
+                    contactPage();
+                    break;
+            };
+        });
+    });
+
     // Create & append content section
-    const content = document.createElement("section");
     content.setAttribute("id", "content");
     document.body.appendChild(content);
 };
